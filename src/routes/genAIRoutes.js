2d66b63079ab3335a31efdc24b3c0cee229534f8
hanseltanoto <13520046@std.stei.itb.ac.id>
2023-12-22 15:45:50 +0700

    fix: gen-ai

--- src/routes/genAIRoutes.js
+++ src/routes/genAIRoutes.js
@@ -3,7 +3,7 @@ import genAIController from "../controllers/genAIController.js";
 
 const router = express.Router();
 
-router.get('/get-recommendations',  genAIController.getListRecommendation)
-router.get('/get-procedure', genAIController.getHowToMake)
+router.post('/get-recommendations',  genAIController.getListRecommendation)
+router.post('/get-procedure', genAIController.getHowToMake)
 
 export default router;
